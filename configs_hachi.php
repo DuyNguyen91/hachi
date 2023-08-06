7704e28c76b77fec14b5876beffabc6bc48a4db0
DuyNguyen91 <duy.nv.hd@gmail.com>
2023-08-06 23:48:26 +0700

    Update configs_hachi.php
    update

--- configs_hachi.php
+++ configs_hachi.php
@@ -9,8 +9,8 @@
         "adsNetCurrentAndroid": "Admob",
         "adsNetCurrentIos": "Admob",
         "max_open_interstitial": 10,
-    	"URL_FILE_ASSETS": "https://sub.helushop.net/hachi",
-    	"API_URL": "https://api74.helushop.net/hachi_v3/public"
+    	"URL_FILE_ASSETS": "https://hachi.helushop.net/assets",
+    	"API_URL": "https://hachi.helushop.net/hachi_v3/public"
         }';
 
     echo $configs;
