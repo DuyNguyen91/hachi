5bfaa372e161e0586aee8c49911066951242eaa7
DuyNguyen91 <duy.nv.hd@gmail.com>
2023-08-06 23:42:22 +0700

    Create configs_hachi.php
    init

---
+++ configs_hachi.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//Admob/FAN/Applovin
+
+    $configs = '{
+        "versionAndroidNewest": 1,
+        "versionIOSNewest": 1,
+        "adsNetCurrentAndroid": "Admob",
+        "adsNetCurrentIos": "Admob",
+        "max_open_interstitial": 10,
+    	"URL_FILE_ASSETS": "https://sub.helushop.net/hachi",
+    	"API_URL": "https://api74.helushop.net/hachi_v3/public"
+        }';
+
+    echo $configs;
+
+?>
